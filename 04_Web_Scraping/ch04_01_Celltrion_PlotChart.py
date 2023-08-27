a12fcf7d46c238d9ce138d0d3f857d69e69ba6d2
semiyong3 <semiyong3@gmail.com>
2023-08-27 16:15:05 +0900

    Update ch04_01_Celltrion_PlotChart.py

--- 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
+++ 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
@@ -18,7 +18,8 @@ sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'
 for page in range(1, int(last_page)+1):
     url = '{}&page={}'.format(sise_url, page)  
     html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
-    df = df.append(pd.read_html(html, header=0)[0])
+    # df = df.append(pd.read_html(html, header=0)[0])
+    df = pd.concat([df, pd.read_html(html, header=0)[0]])
 
 # 차트 출력을 위해 데이터프레임 가공하기
 df = df.dropna()
